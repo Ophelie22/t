876714c5541908ead3bf50cf52bd17e4045b6ff0
Ophelie22 <laly999@hotmail.com>
2023-12-08 20:24:08 +0100

    mise en place de notre fichier style et instalation de Saas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
 
 function App() {
-  
+<div>
+
+</div>
   return <h1>Cookchef</h1>;
 }
 
